85965a33debb2533607c4670b3f9b2b166ceacb4
Sergey <alsedi28@gmail.com>
2018-01-21 15:34:12 +0300

    refactoring ajax_edit_work_plan_page.php and ajax_get_messages.php

--- ajax_get_messages.php
+++ ajax_get_messages.php
@@ -5,7 +5,7 @@ require_once(dirname(__FILE__) . '/helpers.php');
 header('Content-type: application/json');
 
 
-if(!isset($_GET['work_id']) || !isset($_GET['date']) || !isset($_GET['type'])){
+if(!isset($_GET['work_id']) || intval($_GET['work_id']) == 0 || !isset($_GET['date']) || !isset($_GET['type'])){
     echo json_encode(array('status' => "Validation error"));
     exit();
 }
@@ -14,20 +14,21 @@ $work_id = $_GET['work_id'];
 $date = $_GET['date'];
 $type = $_GET['type'];
 
-$sql_nir = "SELECT mdl_nir.id FROM mdl_nir WHERE (mdl_nir.user_id=".$USER->id." OR mdl_nir.teacher_id=".$USER->id.") AND
-                mdl_nir.id=".$work_id." AND mdl_nir.is_closed=0";
-$rs = $DB->get_records_sql($sql_nir);
+$sql_nir = "SELECT mdl_nir.id FROM {nir} WHERE (mdl_nir.user_id = ? OR mdl_nir.teacher_id = ?) AND
+                mdl_nir.id = ? AND mdl_nir.is_closed = 0";
 
-if(count($rs) == 0){
+$nir = $DB->get_record_sql($sql_nir, array($USER->id, $USER->id, $work_id));
+
+if(!$nir){
     echo json_encode(array('status' => "Work does not exist"));
     exit();
 }
 
 $sql_messages = "SELECT * FROM (SELECT mdl_nir_messages.id as message_id, mdl_nir_messages.text, mdl_nir_messages.date, mdl_user.firstname, mdl_user.lastname, mdl_user.id FROM 
-                                mdl_nir_messages, mdl_user WHERE mdl_nir_messages.nir_id=".$work_id." AND mdl_user.id=mdl_nir_messages.user_id AND mdl_nir_messages.nir_type='".$type."' 
-                                AND mdl_nir_messages.date < '".$date."' ORDER BY mdl_nir_messages.id DESC LIMIT 6) as tmp ORDER BY tmp.date";
+                                {nir_messages}, {user} WHERE mdl_nir_messages.nir_id = ? AND mdl_user.id = mdl_nir_messages.user_id AND mdl_nir_messages.nir_type = ? 
+                                AND mdl_nir_messages.date < ? ORDER BY mdl_nir_messages.id DESC LIMIT 6) as tmp ORDER BY tmp.date";
 
-$messages = $DB->get_records_sql($sql_messages);
+$messages = $DB->get_records_sql($sql_messages, array($work_id, $type, $date));
 $count_messages = count($messages);
 
 if($count_messages > 5)
